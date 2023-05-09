895a8a92145eb59c845b738b1971ecd3428d9b40
Gary Clarke <garyclarke1974@gmail.com>
2023-05-09 20:26:50 +0100

    Add TwitterClientTest

---
+++ tests/Http/TwitterClientTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Http;
+
+use PHPUnit\Framework\TestCase;
+
+class TwitterClientTest extends TestCase
+{
+    /** @test */
+    public function getUserById_returns_correctly_formatted_user_data(): void
+    {
+        $this->markTestIncomplete('Work in progress...');
+    }
+}
