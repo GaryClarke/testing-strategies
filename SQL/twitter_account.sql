80b56418a4d4e40ad89e7e1c8d6d7441bcd74edb
Gary Clarke <garyclarke1974@gmail.com>
2023-05-09 19:34:41 +0100

    Add account inserts queries

---
+++ SQL/twitter_account.sql
@@ -0,0 +1,3 @@
+-- twitter_account.sql
+insert into twitter_account (twitter_account_id, follower_count, username, created_at)
+values (19057969, 1000, 'phpunit', '2021-01-01'), (1285294171033604101, 100, 'garyclarketech', '2021-01-01');
