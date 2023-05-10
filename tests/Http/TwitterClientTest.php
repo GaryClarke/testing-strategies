7d8b47d6ad0f088cb489c5de756da2321f32a22c
Gary Clarke <garyclarke1974@gmail.com>
2023-05-10 17:52:29 +0100

    Complete getUserById_returns_correctly_formatted_user_data

--- tests/Http/TwitterClientTest.php
+++ tests/Http/TwitterClientTest.php
@@ -1,14 +1,50 @@
-<?php
+<?php // tests/Http/TwitterClientTest.php
 
 namespace App\Tests\Http;
 
+use App\Http\ApplicationClientInterface;
+use App\Http\TwitterClient;
 use PHPUnit\Framework\TestCase;
 
 class TwitterClientTest extends TestCase
 {
-    /** @test */
+    /**
+     * @test
+     *
+     * We want to test in isolation
+     * There is a dependency which hits a 3rd party service
+     * ApplicationClientInterface makes it easy to switch for a mock
+     * Would not work without an internet connection
+     */
     public function getUserById_returns_correctly_formatted_user_data(): void
     {
-        $this->markTestIncomplete('Work in progress...');
+        // Setup
+        $applicationClient = $this->createMock(ApplicationClientInterface::class);
+        $twitterClient = new TwitterClient($applicationClient);
+        $accountId = 1234;
+
+        $applicationClient->expects($this->once())
+            ->method('get')
+            ->willReturn('{"data":{"name":"PHPUnit","username":"phpunit","public_metrics":
+                {"followers_count":2227,"following_count":0,"tweet_count":525,"listed_count":107},"id":"1234"}}'
+            );
+
+        // Do something
+        $user = $twitterClient->getUserById($accountId);
+
+        // Make assertions
+        $this->assertEquals([
+            "public_metrics" => [
+                "followers_count" => 2227,
+                "following_count" => 0,
+                "tweet_count"     => 525,
+                "listed_count"    => 107,
+            ],
+            "name"            => "PHPUnit",
+            "id"              => "1234",
+            "username"        => "phpunit",
+        ],
+            $user
+        );
     }
 }
